5e7f5f99661d4e067252985616184bf2a40a17e1
Deebya Neupane <dbya.neupane@gmail.com>
2024-03-17 12:20:44 +0545

    initial commit

---
+++ base/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Todo
+
+# Create your views here.
+def home(request):
+    todo = Todo.objects.all()
+    content = {'todos':todo}
+    return render(request,'index.html',context = content)
+    #return HttpResponse('Hello! This is a Home Page')
+ 
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        Todo.objects.create(name=name, description=description, status=status)
+        return redirect('home')
+    return render(request,'create.html')
+
+def edit(request,pk):
+    todo = Todo.objects.get(id=pk)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        todo.name = name
+        todo.description = description
+        todo.status = status
+        todo.save()
+        return redirect('home')
+    content = {'todo':todo}
+    return render(request,'edit.html',context=content)
+
+def delete(request,pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('home')
+
+def delete_all(request):
+    todo = Todo.objects.all()
+    todo.delete()
+    return redirect('home')
